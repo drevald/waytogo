09090c7b97eee07f686ef4000630ea984797f9d3
Denis Dreval <dreval@narod.ru>
2024-07-18 23:08:17 +0300

    server added

--- internal/injectors/ingectors.go
+++ internal/injectors/ingectors.go
@@ -3,6 +3,7 @@ package injectors
 import (
 	"github.com/samber/do"
 	"github.com/ddreval/waytogo/internal/config"
+	"github.com/ddreval/waytogo/internal/servers"
 )
 
 var Default *do.Injector
@@ -10,4 +11,5 @@ var Default *do.Injector
 func init() {
    Default = do.New()
    do.Provide(Default, config.New)
+   do.Provide(Default, server.New)
 }
