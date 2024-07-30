2613d48d401487dfaa1aeed16b68303487941e44
Denis Dreval <dreval@narod.ru>
2024-07-31 00:41:18 +0300

    tidy up

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -19,25 +19,18 @@ func NewAuth(di *do.Injector) (*Auth, error) {
 }
 
 func (auth *Auth) Authenticate(c *gin.Context) {
-
 	auth.logger.Info("Authenticate() called with c.Request.URL.Path = " + c.Request.URL.Path)
-
 	session := sessions.Default(c)
-	fmt.Println("AAA")
 	val := session.Get("user")
 	if c.Request.URL.Path == "/login" || c.Request.URL.Path == "/register" || c.Request.URL.Path == "/favicon.ico" {
-		fmt.Println("BBB")
-		auth.logger.Debug("logger page ok")
+		auth.logger.Warn("logger page ok")
 		c.Next()
 	} else if val != nil { 
-		fmt.Println("CCC")
 		user := val.(*databases.User)
 		auth.logger.Info(fmt.Scanf("Logged as %s", user.Username))
 		c.Next()
 	} else {
-		fmt.Println("DDD")
-		auth.logger.Debug("redirecting to logger page")
+		auth.logger.Error("Unauthorized access - redirecting to login")
 		c.Redirect(301, "login")
 	}	
-	fmt.Println("EEE")
 }
